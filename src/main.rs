4711f2440fa6e1ba17c7886e5a7d2c0f93daac51
L3viathan <git@l3vi.de>
2021-06-16 20:06:52 +0200

    rustfmt ftw, bad original copy&pasting

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::process::{Command, Stdio};
+use clap::{App, AppSettings, Arg};
 use std::io::{self, BufRead};
-use clap::{Arg, App, AppSettings};
+use std::process::{Command, Stdio};
 
 fn main() {
     let matches = App::new("xargh")
@@ -8,21 +8,27 @@ fn main() {
         .version("0.1.0")
         .author("L3viathan <git@l3vi.de>")
         .about("simple xargs replacement")
-        .arg(Arg::with_name("replace")
-                 .takes_value(true)
-                 .long("replace")
-                 .short("r")
-                 .help("Replace this with the input"))
-        .arg(Arg::with_name("shell")
-                 .short("s")
-                 .long("shell")
-                 .help("Whether to shell out (so pipes etc. work)"))
-        .arg(Arg::with_name("command")
-                 .takes_value(true)
-                 .allow_hyphen_values(true)
-                 .multiple(true)
-                 .min_values(1)
-                 .help("The command to run per line of stdin"))
+        .arg(
+            Arg::with_name("replace")
+                .takes_value(true)
+                .long("replace")
+                .short("r")
+                .help("Replace this with the input"),
+        )
+        .arg(
+            Arg::with_name("shell")
+                .short("s")
+                .long("shell")
+                .help("Whether to shell out (so pipes etc. work)"),
+        )
+        .arg(
+            Arg::with_name("command")
+                .takes_value(true)
+                .allow_hyphen_values(true)
+                .multiple(true)
+                .min_values(1)
+                .help("The command to run per line of stdin"),
+        )
         .get_matches();
     let vals: Vec<&str> = matches.values_of("command").unwrap().collect();
     let program = vals[0];
@@ -36,7 +42,7 @@ fn main() {
         Some(value) => value,
         None => "",
     };
-    if ! replace.is_empty() {
+    if !replace.is_empty() {
         let re = matches.value_of("replace").unwrap();
         for line in io::stdin().lock().lines() {
             let text = line.unwrap();
